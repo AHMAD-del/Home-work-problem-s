f7a15bfba284c4841d2a3ee99973ca890eeee456
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2024-12-01 10:22:47 +0500

    Create hw01-q3.py
    Task 3:
    One interesting application of computers is drawing graphs and bar charts
    (sometimes called “histograms”). Write a program that reads five numbers
    (each between 1 and 30). For each number read, your program should print a line
     containing that number of adjacent asterisk.

---
+++ hw01-q3.py
@@ -0,0 +1,13 @@
+"""
+One interesting application of computers is drawing graphs and bar charts
+(sometimes called “histograms”). Write a program that reads five numbers
+(each between 1 and 30). For each number read, your program should print a line
+ containing that number of adjacent asterisk. 
+"""
+lst = []
+count = 1
+while count <= 5:
+    lst.append(int(input(f"Enter number {count}: ")))
+    count +=1
+for i in lst:
+    print("*" * i)    
