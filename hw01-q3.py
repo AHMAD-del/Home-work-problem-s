c1d7fc30e721a4de51681a01559ca1029fb5f3d9
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2024-12-01 22:13:45 +0500

    Update hw01-q3.py

--- hw01-q3.py
+++ hw01-q3.py
@@ -6,8 +6,11 @@ One interesting application of computers is drawing graphs and bar charts
 """
 lst = []
 count = 1
-while count <= 5:
-    lst.append(int(input(f"Enter number {count}: ")))
-    count +=1
+try:
+  while count <= 5:
+     lst.append(int(input(f"Enter number {count}: ")))
+     count +=1
+except:
+ exit("Not a Number!")
 for i in lst:
     print("*" * i)    
