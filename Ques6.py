4f7fac551d6959d7bdc1adbfa45a1098b01cc0e9
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2024-12-16 18:47:22 +0500

    Delete Ques6.py

--- Ques6.py
+++
@@ -1,8 +0,0 @@
-"""
-Task 6:
-  Write a program having a function named greet() that asks the user's name, and then greets the user by name. Before 
-  outputting the user's name, convert it to upper case letters. For example, if the user's name is umair, then the program should 
-  respond "Hello, UMAIR BABAR”
-"""
-name = input("Name: ").uppercase()
-print(f"Hello, {name}")
